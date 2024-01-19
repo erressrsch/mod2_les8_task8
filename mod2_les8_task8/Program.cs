631662070692e66934cb2019a6f9f6c227cc0d42
Szymon Chyc <erressrsch@gmail.com>
2024-01-19 23:38:05 +0100

    Add project files.

---
+++ mod2_les8_task8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string test = "Abcdefg";
+        string revers = string.Empty;
+
+        for (int i = test.Length - 1; i >= 0; i--)
+        {
+            if (i == test.Length - 1)
+            {
+                revers += test[i].ToString().ToUpper();
+
+            }
+            else if (i == 0)
+            {
+                revers = test[i].ToString().ToLower();
+            }
+            else
+            {
+                revers += test[i].ToString();
+            }
+
+
+        }
+        Console.WriteLine($"Input string: {test}");
+        Console.WriteLine($"Output string: {revers}");
+    }
+}
